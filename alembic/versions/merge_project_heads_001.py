5c1d928e371936b3027f61fd307b66839f35b0c5
Francis Akol <akolfrancis40@gmail.com>
2026-03-18 18:37:26 +0200

    fix: add merge migration to resolve multiple head revisions
    Merges add_project_incidents and add_site_visits_progress_handover_001,
    both of which had down_revision pointing to rename_to_payment_certificates.
    Alembic refused to run with 'Multiple head revisions' error.

---
+++ alembic/versions/merge_project_heads_001.py
@@ -0,0 +1,22 @@
+"""Merge project management branch heads
+
+Revision ID: merge_project_heads_001
+Revises: add_project_incidents, add_site_visits_progress_handover_001
+Create Date: 2026-03-18 00:00:00.000000
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = 'merge_project_heads_001'
+down_revision = ('add_project_incidents', 'add_site_visits_progress_handover_001')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
